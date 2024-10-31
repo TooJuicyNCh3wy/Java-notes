95eaed34b55878771f6719d90e42fbc0a67c4628
Andy Alonso <75342059+aalons012@users.noreply.github.com>
2024-10-30 20:32:23 -0400

    Update 4- Using Arrays .java

--- LinkedIn/Java - Data Structures/4- Using Arrays .java
+++ LinkedIn/Java - Data Structures/4- Using Arrays .java
@@ -9,6 +9,15 @@ public class ArrayExamples{
 ```
 remember the class name for this file is also the name of the file
 so
+public class ArrayExamples is also ArrayExamples.java
 
-public class ArrayExamples = ArrayExamples.java
+So in order to Make an Array you can make it any way possible
+Either
+  String
+  Int
+  Double
+  Char
+  Float
+  etc etc
 
+for this lesson we will be using (String)
